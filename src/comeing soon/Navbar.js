2c180b4a04cfb4d1b7c3714c19e167e5c808aae3
govindsoni <govind@arcgate.com>
2022-09-07 19:32:06 +0530

    Initial Commit

---
+++ src/comeing soon/Navbar.js
@@ -0,0 +1,149 @@
+import React, { useState } from "react";
+import "../App.css";
+import Footer from "./Footer";
+import Timer from './Timer';
+const Navbar = () => {
+  const [showLanguage, setShowLanguage] = useState(false);
+  const hello = false;
+
+  const handleClick = () => {
+    // setToggle(false);
+  };
+
+  return (
+    <>
+      <div className="top-container">
+        <div className="header">
+          <div className="nav-start">
+            <ul className="img-dropdown" onClick={() => setShowLanguage(!showLanguage)}>
+              <li>
+                <a className="text_decor" href="" onClick={event => event.preventDefault()}>
+                  <img src="images/flags/US.png" />
+                  <span className="destain">Deutsch</span>
+                </a>
+              </li>
+            </ul>
+            {showLanguage && (
+              <div className="fleg_select">
+                <ul className="dropdown_img">
+                  <li>
+                    <a href="">
+                      <img src="images/flags/DE.png" />
+                      <span className="destain">Deutsch</span>
+                    </a>
+                  </li>
+                  <li>
+                    <a href="">
+                      <img src="images/flags/GB.png" />
+                      <sapn className="destain"> English(UK)</sapn>
+                    </a>
+                  </li>
+                  <li>
+                    <a href="">
+                      <img src="images/flags/FR.png" />
+                      <span className="destain">Français</span>
+                    </a>
+                  </li>
+                  <li>
+                    <a href="">
+                      <img src="images/flags/RO.png" />
+                      <span className="destain">Română</span>
+                    </a>
+                  </li>
+                  <li>
+                    <a href="">
+                      <img src="images/flags/IT.png" />
+                      <span className="destain">Italiano</span>
+                    </a>
+                  </li>
+                  <hr />
+                  <li>
+                    <a href="#">
+                      <img src="images/flags/ES.png" /> Español{" "}
+                      <span className="label label-default">soon</span>
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#">
+                      <img src="images/flags/BR.png" /> Português{" "}
+                      <span className="label label-default">soon</span>
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#">
+                      <img src="images/flags/JP.png" /> 日本語{" "}
+                      <span className="label label-default">soon</span>
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#">
+                      <img src="images/flags/TR.png" /> Türkçe{" "}
+                      <span className="label label-default">soon</span>
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            )}
+          </div>
+          <div className="nav-end">
+            <div className="social-link">
+              <a href="
+              https://www.facebook.com">
+                <i className="fa fa-facebook-square"></i>
+                <span className="link-text"> Like</span>
+              </a>
+            </div>
+            <div className="social-link">
+              <a href="
+              https://twitter.com ">
+                <i className="fa fa-twitter"></i>
+                <span className="link-text"> Tweet</span>
+              </a>
+            </div>
+            <div className="social-link">
+              <a href="
+            https://in.linkedin.com">
+                <i className="fa fa-gittip"></i>
+                <span className="link-text"> Linkdin</span>
+              </a>
+            </div>
+          </div>
+        </div>
+
+        <div className="logo_set">
+          <h1 className="logo-cursive">Restaurant</h1>
+        </div>
+        <div>
+          <h1 className="line-cen-for">
+            The smallest little eatery in town with the biggest taste.
+          </h1>
+          <h5 className="line2-cen-for">
+            Join the waiting list for the beta. We keep you posted.
+          </h5>
+        </div>
+        <div className="form-set">
+          <div>
+            <input
+              type="email"
+              className="form-control"
+              placeholder="Your email here..."
+            />
+          </div>
+          <div className="">
+            {" "}
+            <button type="submit" className="btn btn-warning btn-fill">
+              Notify Me
+            </button>
+          </div>
+        </div>
+        <div>
+        <p className="come">Coming Soon <span>&#9996;</span></p>
+        <Timer/>
+        <Footer/>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Navbar;
---
+++ src/comeing soon/Timer.js
@@ -0,0 +1,35 @@
+import moment from 'moment';
+import React, { useState, useRef, useEffect } from 'react'
+
+
+const App = () => {
+    const calculateTimeLeft = () => {
+        let eventTime = new Date('Thu Sep 14 2022 18:09:32 GMT+0530 (India Standard Time)');
+        let convertTime = Date.parse(eventTime)/1000
+        let currentTime = (Math.floor(Date.now() / 1000)).toString();
+        let leftTime = convertTime - currentTime;
+        let duration = moment.duration(leftTime, 'seconds');
+        let interval = 1000;
+        if (duration.asSeconds() <= 0) {
+            clearInterval(interval);
+            //window.location.reload(true); //#skip the cache and reload the page from the server
+        }
+        duration = moment.duration(duration.asSeconds() - 1, 'seconds');
+        return (duration.days() + ' Days ' + duration.hours()+ ' Hours ' + duration.minutes()+ ' Minutes ' + duration.seconds() + ' Seconds');
+    }
+
+    const [timeLeft, setTimeLeft] = useState(calculateTimeLeft());
+
+    useEffect(() => {
+        setTimeout(() => {
+            setTimeLeft(calculateTimeLeft());
+        },1000);
+    });
+   
+
+    return (
+         
+        <div className='timer'>{timeLeft}</div>
+    )
+}
+export default App;
